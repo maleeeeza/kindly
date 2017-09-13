99420d6441e86789ccad009bd0579df6e3c34192
maleeeeza <melissa.k.schoenberg@gmail.com>
2017-09-12 20:24:07 -0500

    travis ci setup

---
+++ test/test-kindlys.js
@@ -0,0 +1,31 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, runServer, closeServer} = require('../server');
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+
+describe('Kindlys', function() {
+
+  before(function() {
+    return runServer();
+  });
+
+
+  after(function() {
+    return closeServer();
+  });
+
+  it('should return html', function() {
+
+    return chai.request(app)
+      .get('/')
+      .then(function(res) {
+        res.should.have.status(200);
+        res.should.be.html;
+      });
+  });
+  });
