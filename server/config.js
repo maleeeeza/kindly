66fb82840c28cf9929d2da50ef867157bde0496f
maleeeeza <melissa.k.schoenberg@gmail.com>
2017-09-15 13:13:23 -0500

    removed port from localhost

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost:27017/kindly';
+                      'mongodb://localhost/kindly';
 
 googleAPIKey = "AIzaSyCO3ak9ZUiaSK4JOH6ID3TyQYhUQQCVmeI";
