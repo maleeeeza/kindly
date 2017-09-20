695a23cfcb3cb309ab6a7a687d1ec55731cd4f54
maleeeeza <melissa.k.schoenberg@gmail.com>
2017-09-20 10:17:37 -0500

    updated to include prod db

--- server/config.js
+++ server/config.js
@@ -2,9 +2,5 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                       'mongodb://localhost/kindly';
 
-exports.MONGOLAB_URL = process.env.MONGOLAB_URL  ||
-                       global.MONGOLAB_URL ||
-                      'mongodb://ds135234.mlab.com:35234/kindly';
-
 
 googleAPIKey = "AIzaSyCO3ak9ZUiaSK4JOH6ID3TyQYhUQQCVmeI";
