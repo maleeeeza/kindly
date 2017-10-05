0e3c6e4e84405311a6ea1c750b4a6fd9189f97a6
maleeeeza <melissa.k.schoenberg@gmail.com>
2017-10-04 21:34:25 -0500

    updated to remove brackets on user

--- server/users/router.js
+++ server/users/router.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-
-const {User} = require('../models/user');
+const User = require('../models/user');
 
 const router = express.Router();
 
