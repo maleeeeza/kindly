1537183e1eb77065d11727f504534eae2f1ce721
maleeeeza <melissa.k.schoenberg@gmail.com>
2017-10-06 12:36:24 -0500

    tests for get all and get by id routes

--- test/test-kindlys.js
+++ test/test-kindlys.js
@@ -28,4 +28,48 @@ describe('Kindlys', function() {
         res.should.be.html;
       });
   });
+
+  it('should list all kindlys on GET', function() {
+
+    return chai.request(app)
+      .get('/api/kindlys')
+      .then(function(res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.have.property('kindlys').with.length.at.least(1);
+        res.body.kindlys.should.be.a('array');
+        const expectedKeys = ['_id', '__v', 'lat', 'long', 'kindly', 'creator', 'createdDate'];
+        res.body.kindlys.forEach(function(kindly) {
+          kindly.should.be.a('object');
+          kindly.should.include.keys(expectedKeys);
+        });
+      });
+  });
+
+
+  it('should list one kindly on GET by ID', function() {
+
+    return chai.request(app)
+      id = res.body.kindlys[0]._id
+
+      .get(`/api/kindlys/${id}`)
+      .then(function(res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.have.property('kindlys').with.length.at.least(1);
+        res.body.kindlys.should.be.a('array');
+        const expectedKeys = ['_id', '__v', 'lat', 'long', 'kindly', 'creator', 'createdDate'];
+        res.body.kindlys.forEach(function(kindly) {
+          kindly.should.be.a('object');
+          kindly.should.include.keys(expectedKeys);
+        });
+      });
+  });
+
+  
+
+
+
+
+
   });
